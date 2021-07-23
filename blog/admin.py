eb4adb74f16eac85bd0d310160a0590d70891594
SonNavigator <53151107+SonNavigator@users.noreply.github.com>
2021-07-23 12:17:49 +0700

    Update

---
+++ blog/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
+
+
+# Register your models here.
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = '__all__'
+    list_display = [
+        'title',
+        'date_created',
+        'date_updated'
+    ]
+
+
+# admin.site.register(SomeModel, SomeModelAdmin)
+admin.site.register(Post, PostAdmin)
